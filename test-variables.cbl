a9e8a496f031bec1f902ef741ae69f38f8d32d5a
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2025-12-27 13:47:41 -0700

    Prueba de variables

--- test-variables.cbl
+++ test-variables.cbl
@@ -7,19 +7,39 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. TEST-VARIABLES.
        AUTHOR CARLOS IVAN CHAVEZ FUENTES.
-       date-written "25/12/25".
+       DATE-WRITTEN "25/12/25".
+
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
-           01 var1 PICTURE X(5).
-           01 var2 PICTURE X(5).
+           01      var1    PICTURE     9(5).
+           01      var2    PICTURE     9(5).
+           01      result  PICTURE     9(5).
+           01      var1-f  PICTURE     Z(4)9.
+           01      var2-f  PICTURE     Z(4)9.
+           01      result-f PICTURE    Z(4)9.
+
+
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
+           DISPLAY "THIS IS MAIN PROCEDURE"
+           PERFORM FUNCTIONALITY.
+           STOP RUN.
+
+       FUNCTIONALITY.
            DISPLAY "Introduzca el primer valor:"
            ACCEPT var1.
            DISPLAY "Introduzca el segundo valor:"
            ACCEPT var2.
+
+           ADD var1, var2 GIVING result.
+
+           MOVE var1 TO var1-f.
+           MOVE Var2 TO var2-f.
+           MOVE result TO result-f.
+
            DISPLAY "Los valores ingresados fueron: " 
-               FUNCTION TRIM(var1) " y " FUNCTION TRIM(var2).
-           STOP RUN.
+               FUNCTION TRIM(var1-f) " y " FUNCTION TRIM(var2-f)
+               ". Esos valores sumados nos dan: " 
+               FUNCTION TRIM(result-f).
        END PROGRAM TEST-VARIABLES.
